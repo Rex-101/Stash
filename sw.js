828cd5184bfe6fb48874cb3cc8daed07df732d2d
Rex-101 <info.rexence@gmail.com>
2026-06-08 16:42:28 +0100

    bump service worker cache version to force landing page reload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // STASH Service Worker - Minimal for PWA Installation
-const CACHE_NAME = 'stash-v3';
+const CACHE_NAME = 'stash-v4-landing';
 
 // Install
 self.addEventListener('install', (event) => {
